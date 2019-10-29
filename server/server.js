3a96fddb34f0cc07064879c155c85b82f1d6f9ff
Anil Kumar <Anil3.Kumar@igglobal.com>
2019-10-29 15:51:47 +0530

    add files

---
+++ server/server.js
@@ -0,0 +1,66 @@
+import path from 'path'
+import fs from 'fs'
+
+import express from 'express'
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import { StaticRouter } from 'react-router-dom';
+
+import App from '../src/App'
+
+const PORT = 8080
+const app = express()
+
+const router = express.Router()
+
+// 
+app.get('/*', (req, res) => {
+    const context = {};
+    const app = ReactDOMServer.renderToString(
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+    );
+  
+    const indexFile = path.resolve('./build/index.html');
+    fs.readFile(indexFile, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Something went wrong:', err);
+        return res.status(500).send('Oops, better luck next time!');
+      }
+  
+      return res.send(
+        data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+      );
+    });
+  });
+
+
+//   
+// const serverRenderer = (req, res, next) => {
+//   fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(err)
+//       return res.status(500).send('An error occurred')
+//     }
+//     return res.send(
+//       data.replace(
+//         '<div id="root"></div>',
+//         `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+//       )
+//     )
+//   })
+// }
+// router.use('^/$', serverRenderer)
+
+router.use(
+  express.static(path.resolve(__dirname, '..', 'build'), { maxAge: '30d' })
+)
+
+// tell the app to use the above rules
+app.use(router)
+
+// app.use(express.static('./build'))
+app.listen(PORT, () => {
+  console.log(`SSR running on port ${PORT}`)
+})
---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+     
+      <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+      </header>
+     
+    </div>
+  );
+}
+
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export default App;
